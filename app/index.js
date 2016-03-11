a23457729f209ebce2bb76c9f36f21b0e6b020d2
Noah Levine <Noah@Noahs-MacBook-Pro.local>
2016-03-10 21:21:07 -0800

    removed unnecessary border radius from certain buttons, made the app only show up in google chrome because it's the only good browser smh

--- app/index.js
+++ app/index.js
@@ -7,15 +7,22 @@ import collageBoardApp from './reducers/index'
 import App from './components/App'
 import initialState from '../scratchpad/state-structure'
 
-let store = createStore(
-	collageBoardApp,
-	initialState,
-	applyMiddleware(thunkMiddleware)
-)
+if (window.chrome) {
+	let store = createStore(
+		collageBoardApp,
+		initialState,
+		applyMiddleware(thunkMiddleware)
+	)
 
-render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
-	document.getElementById('root')
-)
+	render(
+		<Provider store={store}>
+			<App />
+		</Provider>,
+		document.getElementById('root')
+	)
+} else {
+	let message = document.createElement('h1')
+	message.innerText = 'Please visit this page using Google Chrome for desktop'
+	document.getElementById('root').appendChild(message)
+}
+
